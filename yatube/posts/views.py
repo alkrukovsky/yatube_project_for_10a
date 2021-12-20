da78677b02bc31fb853193c18a4e413bdcae605b
Alexandr <green_83@list.ru>
2021-12-20 09:44:03 +0300

    add context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,22 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Посты группы {slug}')
+    template = 'posts/group_list.html'
+    title = 'Посты выбранной группы'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
